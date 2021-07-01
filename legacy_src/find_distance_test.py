0353b0944f522a6db4cca5d824b74a7360a25cc3
Beksultan Tuleev <beksultan.tuleev@studenti.unitn.it>
2021-07-01 15:50:13 +0100

    minor bugs eliminated

---
+++ legacy_src/find_distance_test.py
@@ -0,0 +1,19 @@
+from rssi_module.rssi import RSSI_Localizer, RSSI_Scan
+from rssi_module import rssi
+accessPoint = {
+     'signalAttenuation': 3, 
+     'location': {
+         'y': 10, 
+         'x': 0
+     }, 
+     'reference': {
+         'distance': 4, 
+         'signal': -52
+     }, 
+     'name': 'dd-wrt'
+}
+signalStrength = -53
+
+distance = RSSI_Localizer.getDistanceFromAP(accessPoint, signalStrength)
+print(distance)
+# RSSI_Localizer.getNodePosition()
