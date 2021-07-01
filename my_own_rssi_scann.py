c751d6f38929604c785eace93b2555df1b6771df
Beksultan Tuleev <beksultan.tuleev@studenti.unitn.it>
2021-07-01 15:50:25 +0100

    com

--- my_own_rssi_scann.py
+++
@@ -1,7 +0,0 @@
-"support has stopped at this moment"
-
-from subprocess import Popen, PIPE
-scan_command = ['sudo','iwlist',"wlo1",'scan']
-scan_process = Popen(scan_command, stdout=PIPE, stderr=PIPE)
-(raw_output, raw_error) = scan_process.communicate() 
-print(raw_output)
